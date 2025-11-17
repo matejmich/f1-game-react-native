a5d6015b4011b8cf97a39c5669e5114ddeabdc7d
matejmich <matejmich@seznam.cz>
2025-11-17 09:38:10 +0100

    feat: first page styling

--- src/constants/colors.tsx
+++ src/constants/colors.tsx
@@ -2,5 +2,6 @@ const colors = {
   primary: '#15151E',
   secondary: '#FF1E00',
   white: '#FFFFFF',
+  transparent: 'transparent',
 };
 export default colors;
--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,6 +1,7 @@
 import { ImageBackground } from 'expo-image';
+import { LinearGradient } from 'expo-linear-gradient';
 import { router } from 'expo-router';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import backgroudImage from '../../assets/images/welcome_photo.jpg';
 
@@ -15,7 +16,15 @@ export default function WelcomeScreen() {
     <>
       <ImageBackground source={backgroudImage} contentFit="cover" style={styles.background}>
         <SafeAreaView style={styles.container}>
-          <Text style={styles.text}>Welcome to F1 mini 🏁</Text>
+          <View style={styles.titleContainer}>
+            <Text style={styles.text}>F1 mini 🏁</Text>
+            <LinearGradient
+              colors={[colors.transparent, colors.secondary, colors.transparent]}
+              start={{ x: 0, y: 0.9 }}
+              end={{ x: 1, y: 0.0 }}
+              style={styles.hrGradient}
+            />
+          </View>
           <TouchableOpacity style={styles.button} onPress={handleNavigation}>
             <Text style={styles.buttonText}>Play</Text>
           </TouchableOpacity>
@@ -40,13 +49,19 @@ const styles = StyleSheet.create({
   },
   text: {
     color: colors.white,
-    fontSize: 24,
+    fontSize: 48,
     fontFamily: 'Orbitron-Regular',
   },
   button: {
     padding: 32,
-    backgroundColor: colors.secondary,
-    borderRadius: 8,
+    backgroundColor: colors.primary,
+    borderColor: colors.secondary,
+    borderWidth: 2,
+    borderRadius: 16,
+    shadowColor: colors.secondary,
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.8,
+    shadowRadius: 32,
   },
   buttonText: {
     color: colors.white,
@@ -54,5 +69,13 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     fontFamily: 'Orbitron-ExtraBold',
     textTransform: 'uppercase',
+  },
+  titleContainer: {
+    alignItems: 'center',
+    gap: 0,
+  },
+  hrGradient: {
+    width: 400,
+    height: 4,
   },
 });
